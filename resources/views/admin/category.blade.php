842d76bd4ff5c0fb299abb53994747e9ca3c357f
unknown <kafiasadeq25>
2024-05-30 19:23:31 +0300

    Show Sweet Alert Delete Confirmation

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -63,7 +63,7 @@
                   @foreach ($data as $categoryName)
                     <tr>
                       <td>{{ $categoryName->category_name}}</td>
-                      <td><a href="{{url('delete_category',$categoryName->id)}}" class="btn btn-danger">Delete</a></td>
+                      <td><a onclick="confirmation(event)" href="{{url('delete_category',$categoryName->id)}}" class="btn btn-danger">Delete</a></td>
                     </tr>
                   @endforeach
                 </table>
@@ -72,6 +72,30 @@
       </div>
     </div>
     <!-- JavaScript files-->
+    <script>
+      function confirmation(ev){
+        ev.preventDefault();
+        var urlRedirect=ev.currentTarget.getAttribute('href');
+        console.log(urlRedirect);
+
+        swal({
+        title: "Are you sure Delete this ",
+        text: "this delete will be aparmanent",
+        icon: "warning",
+        buttons:true,
+        dangerModel:true
+      })
+
+       .then((willCancel)=>{
+      if(willCancel){
+          window.location.href=urlRedirect;
+      }
+      });
+      }
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" 
+    integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"> </script>
     <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -15,6 +15,7 @@
       </div>
     </div>
     <!-- JavaScript files-->
+    
     <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"> </script>
     <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
