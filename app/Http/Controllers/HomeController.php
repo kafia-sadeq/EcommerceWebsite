63ea612b7c266b163b415d109b704248efa820b6
unknown <kafiasadeq25>
2024-06-13 13:50:45 +0300

    Display Product in Home Page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
@@ -10,6 +11,7 @@ class HomeController extends Controller
      return view('admin.index');
     }
    public function home(){
-    return view('home.index');
+    $products=Product::all();
+    return view('home.index',compact('products'));
    }
 }
